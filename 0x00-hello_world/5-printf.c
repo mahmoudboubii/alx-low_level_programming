5c9cf942c2edd8692cebd90bb5aaf91730e44483
mahmoudboubii <boubiimahmoud@gmail.com>
2023-03-16 14:32:50 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-/**
-* main - this is the main function
-*
-* Return: 0 when successfull
-*/
-
 int main(void)
 {
-printf("with proper grammer, but the outcome is a piece of art,");
-retun (0);
+    printf("Size of a char: %lu byte(s)\n", sizeof(char));
+    printf("Size of an int: %lu byte(s)\n", sizeof(int));
+    printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+    printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+    printf("Size of a float: %lu byte(s)\n", sizeof(float));
+    printf("Size of a double: %lu byte(s)\n", sizeof(double));
+    printf("Size of a long double: %lu byte(s)\n", sizeof(long double));
+    printf("Size of a void pointer: %lu byte(s)\n", sizeof(void *));
+    return 0;
 }
