918bdc2d36e65f000e373b7dee9b1efc662cdec3
mahmoudboubii <boubiimahmoud@gmail.com>
2023-03-17 03:15:58 -0700

    edit_2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,11 +2,10 @@
 
 int main(void)
 {
-char c;
+    char c;
 
-for (c = 'a'; c <= 'z'; c++)
-putchar(c);
-putchar('\n');
+    for (c = 'a'; c <= 'z'; ++c)
+        putchar(c);
 
-return 0;
+    return (0);
 }
